6faf5a9cd32f3a164f05aa8211f00306e6c2c098
Kripta-Studios <68393696+Kripta-Studios@users.noreply.github.com>
2025-08-16 17:17:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ DISCORD_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="#", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -39,3 +39,4 @@ async def load(ctx, ticker: str, expiration: str, greek: str):
 
 def run_bot():
     bot.run(DISCORD_TOKEN)
+
